cdb1dfdb4b90c975df311e6504cbfdb9eba4fd99
yechan <noy3928@naver.com>
2022-07-22 11:02:07 +0900

    DoublyLinkedList - push method

---
+++ ComputerScience/DataStructure/DoublyLinkedList/desc.md
@@ -0,0 +1,8 @@
+# Push
+
+- create a new node with the value passed to the function
+- if the head property is null set the head and tail to be the newly created node
+- if not, set the next property on the tail to be that node
+- set the previous property on the newly created node to be the tail
+- set the tail to be the newly created node
+- increment the length
---
+++ ComputerScience/DataStructure/DoublyLinkedList/doublyLinkedList.js
@@ -0,0 +1,28 @@
+class Node {
+  constructor(val) {
+    this.val = val
+    this.next = null
+    this.prev = null
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+  push(val) {
+    const newNode = new Node(val)
+    if (!this.head) {
+      this.head = newNode
+      this.tail = this.head
+    } else {
+      this.tail.next = newNode
+      newNode.prev = this.tail
+      this.tail = newNode
+    }
+    this.length++
+    return this
+  }
+}
