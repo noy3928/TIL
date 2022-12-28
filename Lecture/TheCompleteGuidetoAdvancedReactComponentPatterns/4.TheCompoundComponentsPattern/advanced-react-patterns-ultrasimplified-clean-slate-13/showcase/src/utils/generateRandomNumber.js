af485f895de6f337b0b6d05dc2d1a34afffd092c
노예찬 <71473074+noy3928@users.noreply.github.com>
2022-12-28 15:18:26 +0900

    remove unnecessery code

--- Lecture/TheCompleteGuidetoAdvancedReactComponentPatterns/4.TheCompoundComponentsPattern/advanced-react-patterns-ultrasimplified-clean-slate-13/showcase/src/utils/generateRandomNumber.js
+++
@@ -1,2 +0,0 @@
-export const generateRandomNumber = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min)
--- Lecture/TheCompleteGuidetoAdvancedReactComponentPatterns/4.TheCompoundComponentsPattern/advanced-react-patterns-ultrasimplified-clean-slate-13/showcase/webpack.config.prod.js
+++
@@ -1,6 +0,0 @@
-const merge = require('webpack-merge')
-const baseConfig = require('./webpack.config.base')
-
-module.exports = merge(baseConfig, {
-  mode: 'production'
-})
