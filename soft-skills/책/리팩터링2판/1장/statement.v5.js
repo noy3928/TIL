f798a3849c6249ec625a56e4a69842d5509fdea3
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-11-08 08:46:29 +0900

    total amount 함수 추출하기

---
+++ soft-skills/책/리팩터링2판/1장/statement.v5.js
@@ -0,0 +1,72 @@
+function volumeCredits(perf) {
+  let result = 0;
+  result += Math.max(perf.audience - 30, 0);
+  if ("comedy" === playFor(perf).type) result += Math.floor(perf.audience / 5);
+  return result;
+}
+
+function playFor(aPerformance) {
+  return plays[aPerformance.playID];
+}
+
+function amountFor(aPerformance) {
+  let result = 0;
+
+  switch (playFor(aPerformance).type) {
+    case "tragedy":
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
+      }
+      break;
+    case "comedy":
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
+      }
+      result += 300 * aPerformance.audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+  }
+
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
+}
+
+function totalVolumeCredits() {
+  let result = 0;
+  for (let perf of invoice.performances) {
+    result += volumeCredits(perf);
+  }
+  return result;
+}
+
+function totalAmount() {
+  let result = 0;
+  for (let perf of invoice.performances) {
+    result += amountFor(perf);
+  }
+  return result;
+}
+
+function statement(invoice, plays) {
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+
+  for (let perf of invoice.performances) {
+    result += `${play.name}: ${usd(amountFor(perf) / 100)} (${
+      perf.audience
+    }석)\n`;
+  }
+
+  result += `총액: ${usd(totalAmount() / 100)}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  return result;
+}
