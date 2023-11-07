38a231cc54d122a4ce63136a2e4f4aca474c25ef
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-11-08 08:39:04 +0900

    함수 추출하기, 선언 바꾸기

--- soft-skills/책/리팩터링2판/1장/statement.v4.js
+++ soft-skills/책/리팩터링2판/1장/statement.v4.js
@@ -33,27 +33,30 @@ function amountFor(aPerformance) {
   return result;
 }
 
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
+}
+
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCredits(perf);
 
     // 청구 내역을 출력한다.
-    result += `${play.name}: ${format(amountFor(perf) / 100)} (${
+    result += `${play.name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     }석)\n`;
     totalAmount += thisAmount;
   }
 
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 }
