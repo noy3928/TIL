db48b4a95b29dfd59501e0d0d5be60d0e60b3692
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-11-08 08:43:14 +0900

    from obsidian : 2023-11-08 08:43:13
    Affected files:
    soft-skills/책/리팩터링2판/1장/statement.v4.js

--- soft-skills/책/리팩터링2판/1장/statement.v4.js
+++ soft-skills/책/리팩터링2판/1장/statement.v4.js
@@ -41,14 +41,19 @@ function usd(aNumber) {
   }).format(aNumber / 100);
 }
 
+function totalVolumeCredits() {
+  let result = 0;
+  for (let perf of invoice.performances) {
+    result += volumeCredits(perf);
+  }
+  return result;
+}
+
 function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCredits(perf);
-
     // 청구 내역을 출력한다.
     result += `${play.name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
@@ -57,6 +62,14 @@ function statement(invoice, plays) {
   }
 
   result += `총액: ${usd(totalAmount / 100)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 }
+
+/*
+1.반복문 쪼개기 
+2. 문장 슬라이드하기 : 변수 초기화 문장을 변수 값 누적 코드 바로 압ㅍ으로 옮긴다. 
+3. 함수 추출하기 : 적립 포인트 계산 부분을 별도 함수로 추출한다. 
+4. 변수 인라인하기 : volumeCredits 변수를 제거한다.
+
+*/
