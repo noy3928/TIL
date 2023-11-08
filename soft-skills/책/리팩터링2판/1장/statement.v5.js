043002b60f683496afbc651cf81414bba12ac90f
noyechan <noy3928@naver.com>
2023-11-08 19:35:52 +0900

    from obsidian : 2023-11-08 19:35:52
    Affected files:
    soft-skills/책/리팩터링2판/1장/statement.v5.js

--- soft-skills/책/리팩터링2판/1장/statement.v5.js
+++ soft-skills/책/리팩터링2판/1장/statement.v5.js
@@ -80,7 +80,7 @@ function statement(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
-    result.volumeCredits = volumeCredits(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
@@ -105,6 +105,14 @@ function statement(invoice, plays) {
         throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
     }
 
+    return result;
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === aPerformance.play.type)
+      result += Math.floor(aPerformance.audience / 5);
     return result;
   }
 
