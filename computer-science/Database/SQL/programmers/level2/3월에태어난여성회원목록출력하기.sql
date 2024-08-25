073d1282b5b4e6db6de1c1433c5c5570c0b7380b
노예찬 <71473074+noy3928@users.noreply.github.com>
2024-08-26 00:11:19 +0900

    자바의 정석 변수

---
+++ computer-science/Database/SQL/programmers/level2/3월에태어난여성회원목록출력하기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, "%Y-%m-%d") as DATE_OF_BIRTH
+from MEMBER_PROFILE
+where MONTH(DATE_OF_BIRTH) = "03"
+and GENDER = "W" 
+and TLNO is not null 
+order by MEMBER_ID
+
